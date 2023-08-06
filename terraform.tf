cb78fd3de3ef5ff3f477c2164f22b603f0f1c700
mallikarjuna kukatikonda <[200~>
2023-08-06 18:10:48 +0530

    port8081

--- terraform.tf
+++ terraform.tf
@@ -21,6 +21,12 @@ resource "aws_security_group" "inbound_rules" {
         protocol = "tcp"
         cidr_blocks = ["0.0.0.0/0"]
     }
+    ingress {
+        from_port = 8081
+        to_port = 8081
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
 }
 resource "aws_instance" "Django" {
     
